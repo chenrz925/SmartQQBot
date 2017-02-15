6dc5989a8106abac13f13e25a03616b6dfea295e
chenrz925 <chenrz925@live.cn>
2017-02-15 14:56:53 +0800

    Revert "Merge branch 'chenrz925_iwish'"
    This reverts commit 86a0c70fc8a59f22c277e389cf5be15c06f54752, reversing
    changes made to 3e15e4720d1c149b730eda03207f47940e828e8f.

--- src/smart_qq_plugins/turing_robot.py
+++ src/smart_qq_plugins/turing_robot.py
@@ -1,24 +1,15 @@
 # coding: utf-8
 from random import randint
-from smart_qq_bot.logger import logger
 import requests
-import hashlib
 
-from smart_qq_bot.signals import (
-    on_all_message,
-    on_discuss_message,
-    on_group_message
-)
-from smart_qq_bot.messages import GROUP_MSG;
-import datetime
+from smart_qq_bot.signals import on_all_message
 
 # 使用前请先前往 http://www.tuling123.com/register/index.jhtml
 # 申请 API key 谢谢
 # 另外需要 requests 支持
 # 修改成调用图灵官方接口
 url = 'http://www.tuling123.com/openapi/api'
-apikey = '8124ed4f28874af6ac759fc48f4fff99'
-
+apikey = ''
 
 @on_all_message
 def turing_robot(msg, bot):
@@ -26,29 +17,13 @@ def turing_robot(msg, bot):
     :type bot: smart_qq_bot.bot.QQBot
     :type msg: smart_qq_bot.messages.QMessage
     """
-    if datetime.datetime.now().minute % 2 == 0:
-        try:
-            querystring = {
-                "key": apikey,
-                "info": msg.content,
-                "userid": msg.src_sender_id
-            }
-            response = requests.request("GET", url, params=querystring)
-            response_json = response.json()
-            bot.reply_msg(msg, response_json.get('text'))
-            try:
-                bot.reply_msg(msg, response_json.get('url'))
-            except AttributeError:
-                logger.info("No URL!")
-        except NotImplementedError:
-            querystring = {
-                "key": apikey,
-                "info": msg.content
-            }
-            response = requests.get(url, params=querystring);
-            response_json = response.json()
-            bot.reply_msg(msg, response_json.get('text'))
-            try:
-                bot.reply_msg(msg, response_json.get('url'))
-            except AttributeError:
-                logger.info("No URL!")
+
+    querystring = {
+        "key": apikey,
+        "info": msg.content,
+    }
+
+    response = requests.request("GET", url, params=querystring)
+
+    response_json = response.json()
+    bot.reply_msg(msg, response_json.get('text'))
